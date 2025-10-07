db063bf13f5aa6f494d20124e753de146682bd67
Thomas Fx <tf.it@tuta.io>
2025-10-07 12:15:46 +0000

    clean and refactor

--- deploy.sh
+++ deploy.sh
@@ -8,16 +8,16 @@ fi
 
 # Génère le certificat SSL
 echo "Déplacement dans le répertoire openssl..."
-cd /home/thomas/docker/openssl || { echo "Échec du déplacement dans le répertoire openssl"; exit 1; }
+cd /home/admintf/ynov_docker/openssl || { echo "Échec du déplacement dans le répertoire openssl"; exit 1; }
 
 echo "Génération du certificat SSL..."
 docker compose -f docker-compose.yml run --rm openssl || { echo "Échec de la génération du certificat SSL"; exit 1; }
 
 # Déploie GitLab
-echo "Déplacement dans le répertoire gitlab..."
-cd /home/thomas/docker/gitlab || { echo "Échec du déplacement dans le répertoire gitlab"; exit 1; }
+echo "Déplacement dans le répertoire Gitlab..."
+cd /home/admintf/ynov_docker/gitlab || { echo "Échec du déplacement dans le répertoire gitlab"; exit 1; }
 
 echo "Déploiement de GitLab..."
-docker stack deploy -c docker-compose.yml gitlab || { echo "Échec du déploiement de GitLab"; exit 1; }
+docker compose -f docker-compose.yml up -d || { echo "Échec du déploiement de GitLab"; exit 1; }
 
 echo "Script exécuté avec succès."
